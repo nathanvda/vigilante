3a9b15fa26d2f4838b5f13dd2266fdd0e1f2f50c
nathanvda <nathan@dixis.com>
2020-04-03 13:02:33 +0200

    Allow to access operators from an ability
    This will allow us to e.g. list the (number of) users
    having a role.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,8 @@
 class Ability < ActiveRecord::Base
   has_many :ability_permissions
   has_many :permissions, :through => :ability_permissions
+  has_many :authorizations
+  has_many :operators, :through => :authorizations
 
   accepts_nested_attributes_for :permissions
   accepts_nested_attributes_for :ability_permissions
